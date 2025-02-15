d21cd635360cbd705839669299a8f7f42029a9a5
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-02-15 20:06:57 +0600

    work with dot banner ready

---
+++ src/app/work-with-dot/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import React from "react";
+import DotImage from "../../assets/images/work-with-Dot.png";
+
+const WorkWithDot = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex items-end">
+        <Image className="w-[980px]" src={DotImage} alt="dot Image" />
+        <div className="-ml-40">
+          <h1 className="text-[80px] font-extrabold font-gilroy leading-[85px]">WORK <br /> WITH DOT</h1>
+          <p className="font-caslon text-lg w-[380px] mt-5 mb-14"><span className="italic text-secondary">Mythical digital magic.</span> I have worked worldwide weaving wonders for top brands, events, and concepts.</p>
+          <p className="font-gilroy text-sm">EXPLORE</p>
+          <div className="w-[1px] h-24 bg-secondary ml-6 mt-5" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkWithDot;
