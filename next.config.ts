9f9439e7ed121541ef20d50593ab120a0b64ede1
Rafi <itzmeh320@gmail.com>
2025-03-09 23:42:18 +0600

    post api

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dotlung.com", // Change this to match your WordPress domain
+        pathname: "/wp-content/uploads/**",
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   }
