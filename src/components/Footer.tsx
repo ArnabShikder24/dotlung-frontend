b600628ff8604f475c62ed0c29da0109252b0ee2
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-03-11 19:46:51 +0600

    work-with-dot responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const EmailInput = () => {
   return (
-    <div className="flex items-center border border-white px-3 py-2 w-[350px] mt-5">
+    <div className="flex items-center border border-white px-3 py-2 w-[100%] lg:w-[350px] mt-5">
       <input
         type="email"
         placeholder="Your e-mail"
@@ -19,12 +19,12 @@ const EmailInput = () => {
 const Footer = () => {
   return (
     <footer className="w-full p-4 font-gilroy mt-5 relative">
-      <Image src={model1} alt='' width={280} className="absolute bottom-10 right-10 opacity-[1]" />
+      <Image src={model1} alt='' width={280} className="absolute bottom-10 right-10 opacity-[1] hidden lg:block" />
       <div className="flex items-center justify-center">
         <div className="max-w-3xl flex-1 mx-20  flex justify-center">
           <div className="  flex w-full">
-            <div style={{width:"85%"}}>
-              <div className="flex justify-between">
+            <div className="mx-auto w-full lg:w-[85%]">
+              <div className="lg:flex justify-between">
                 <div>
                   <h1 className="text-5xl font-bold">LET’S TALK</h1>
                 </div>
@@ -33,7 +33,7 @@ const Footer = () => {
                   <p className="text-lg font-caslon">hello@dotlung.com</p>
                 </div>
               </div>
-              <div className="flex justify-between mt-10">
+              <div className="lg:flex justify-between mt-10">
                 <div>
                   <p className="italic text-secondary mt-4">
                     Don’t miss out on this!
@@ -66,7 +66,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="py-2 px-6 flex justify-between items-center">
+      <div className="py-2 px-6 text-center lg:flex justify-between items-center">
         <div>
           <Link href="/blog" className="font-bold text-sm hover:text-indigo-200 transition-colors">
             BLOG
