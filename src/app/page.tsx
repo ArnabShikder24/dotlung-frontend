6363ee6abdc298edd583546dc3933a1ddbaf5d67
ArnabShikder24 <arnabshikder2001@gmail.com>
2025-04-03 21:51:42 +0600

    home page fixing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           className="h-auto lg:w-[390px] mx-auto lg:-ml-[300px]"
         >
           <div className="z-10 text-left max-w-2xl px-6">
-            <h1 className="text-5xl font-bold uppercase">
+            <h1 className="text-4xl lg:text-5xl font-bold uppercase">
               Hey, I am Dot. Mother of social media dragons
             </h1>
             <p className="font-caslon mt-8 mb-12">
@@ -35,13 +35,13 @@ export default function Home() {
             <p className="flex items-center gap-6 mt-6 py-3 text-white font-light font-gilroy" style={{fontSize:"12px"}}>
               Comm&apos;on Chase the Dragon!
               <span
-                className="block w-24 h-[2.5px] bg-red-600"
+                className="block w-24 h-[1.5px] bg-red-600"
               ></span>
             </p>
           </div>
         </div>
       </div>
-      <div className="absolute left-0 bottom-5 w-full">
+      <div className="absolute left-0 bottom-4 w-full">
         <div className="flex items-center justify-between px-5">
           <Link href="/blog" className="font-bold text-sm hover:text-indigo-200">
             BLOG
